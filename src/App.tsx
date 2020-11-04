655d70d5a888bcb17eedc1bf0a41e6f7d949520c
Lukasz Grela <lukasz.grela@gmail.com>
2020-11-04 14:26:07 +0000

    Use Console

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
-import React from 'react';
-import { start as toneStart, context as toneContext } from 'tone';
-import './App.css';
-import Grid from './components/Grid/Grid';
-import { useMount } from './hooks/useMount';
+import React from "react";
+import { start as toneStart, context as toneContext } from "tone";
+import "./App.css";
+import Console from "./components/Console/Console";
+import Grid from "./components/Grid/Grid";
+import { useMount } from "./hooks/useMount";
+import Instruments from "./Instruments/Instruments";
 
 const App: React.FC = (): JSX.Element => {
   const [initiated, setInitiated] = React.useState(false);
   const [loading, setLoading] = React.useState(false);
+  const players = React.useRef<Instruments>(new Instruments());
 
   // const downHandler = (e: KeyboardEvent): void => {  };
   // const upHandler = (e: KeyboardEvent): void => {};
@@ -20,18 +23,25 @@ const App: React.FC = (): JSX.Element => {
   });
 
   return (
-    <div className='App'>
-      <header className='App-header'>
+    <div className="App">
+      <header className="App-header">
         <p>Beat Box Maker</p>
       </header>
 
-      <section className='App-section'>
-        <div className='beats'>
+      <section className="App-section">
+        <div className="beats">
           {loading && <p>LOADING...</p>}
-          {!loading && initiated && <Grid />}
+          {!loading && initiated && (
+            <Console
+              instruments={players.current.instruments}
+              beats={[...Array(9)].map(() =>
+                [...Array(32)].map((i) => (Math.random() > 0.5 ? 0 : 1))
+              )}
+            />
+          )}
           {!loading && !initiated && (
             <button
-              className='tap-to-start'
+              className="tap-to-start"
               onClick={async () => {
                 await toneStart();
                 toneContext.resume();
